e71872b1c867ef533b4dfea0c59aedaefb22f03c
abutt1234 <87237147+abutt1234@users.noreply.github.com>
2021-07-11 11:40:12 -0400

    Update gittest.tf

--- gittest.tf
+++ gittest.tf
@@ -3,7 +3,7 @@ region = "us-east-2"
 access_key = "AKIAY2G6N2BCXB2RLYGO"
 secret_key = "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"
 }
-resource "aws_s3_bucket" "bucket123" {
+resource "aws_s3_bucket" "bucket1234" {
 bucket = "asim123098wsx123"
 acl = "private"
 }
