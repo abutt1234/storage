95a7da0506d1ae39f9c277589ac09c7d560d6a86
abutt1234 <87237147+abutt1234@users.noreply.github.com>
2021-07-11 11:05:47 -0400

    Update gittest.tf

--- gittest.tf
+++ gittest.tf
@@ -1,20 +1,10 @@
 provider "aws" {
 region = "us-east-2"
-access_key = ""
-secret_key = ""
+access_key = "AKIAY2G6N2BCXB2RLYGO"
+secret_key = "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"
 }
 resource "aws_s3_bucket" "bucket123" {
 bucket = "asim123098wsx"
 acl = "private"
 }
-resource "aws_db_instance" "asim-098" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mysql890"
-  username             = "admin"
-  password             = "admin123"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-}
+
